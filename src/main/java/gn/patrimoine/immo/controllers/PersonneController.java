93dde3726d13e9f925f29c755d22ff709b188364
user <thiernaliou@gmail.com>
2025-08-03 19:05:34 +0200

    correction

--- src/main/java/gn/patrimoine/immo/controllers/PersonneController.java
+++ src/main/java/gn/patrimoine/immo/controllers/PersonneController.java
@@ -3,7 +3,6 @@
  */
 package gn.patrimoine.immo.controllers;
 
-import org.hibernate.annotations.processing.Pattern;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import gn.patrimoine.immo.dto.PersonneDto;
